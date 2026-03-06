cb1025a3fd8bd6ba364bd794b78d4231bc5bc999
Hongchen Li <hongche4@ualberta.ca>
2026-03-05 21:30:53 -0700

    lab8 first

---
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -0,0 +1,13 @@
+package com.example.lab8;
+
+import java.util.ArrayList;
+
+public class CustomList {
+
+    private ArrayList<City> cities;
+
+    public CustomList() {
+        cities = new ArrayList<>();
+    }
+
+}
