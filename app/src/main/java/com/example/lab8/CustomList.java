d6f5576ee6739f55c96d53d8fd1e57740ec3bfe2
Hongchen Li <hongche4@ualberta.ca>
2026-03-05 21:34:44 -0700

    Add failing test for deleteCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -3,11 +3,17 @@ package com.example.lab8;
 import java.util.ArrayList;
 
 public class CustomList {
-
     private ArrayList<City> cities;
 
     public CustomList() {
         cities = new ArrayList<>();
     }
 
+    public void addCity(City city) {
+        cities.add(city);
+    }
+
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 }
