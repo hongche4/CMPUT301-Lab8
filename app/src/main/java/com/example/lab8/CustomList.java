aa47f43160894d42a3a898ab3c11d5cab0b8e3a8
Hongchen Li <hongche4@ualberta.ca>
2026-03-05 21:41:44 -0700

    Implement deleteCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public class CustomList {
     private ArrayList<City> cities;
-
     public CustomList() {
         cities = new ArrayList<>();
     }
-
     public void addCity(City city) {
         cities.add(city);
     }
-
     public boolean hasCity(City city) {
         return cities.contains(city);
+    }
+    public void deleteCity(City city) {
+        cities.remove(city);
     }
 }
